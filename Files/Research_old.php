d75c90195a74c8b9acd078ea83fea07e5d9686df
Anne Jorstad <anne.jorstad@epfl.ch>
2016-02-28 20:18:07 +0100

    Added files via upload

---
+++ Files/Research_old.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<link type="text/css" rel="stylesheet" href="style.css">
+<title>Anne Jorstad</title>
+<meta name="author" content="Anne Jorstad">
+<meta name="description" content="Anne Jorstad">
+</head>
+
+<body>
+<table style="width:100%">
+
+
+<!-- Navigator bar -->
+<tr>
+	<td>
+	<center>
+	<div class="navstyle">
+	<table style="width:800px">
+		<tr>
+		<!-- <td class="navelt"><a href="http://cvlab.epfl.ch/~jorstad/">Home</a></td>  <!-- http://cvlabwww.epfl.ch/~jorstad/ -->
+		<td class="navelt"><a href="index">Home</a></td>
+		<td class="navelt"><a href="Research">Research</a></td>
+		<td class="navelt"><a href="Teaching">Teaching</a></td>
+		<td class="navelt"><a href="Files/AnneJorstadCV.pdf">CV</a></td>
+		</tr>
+	</table>
+	</div>
+	</center>
+	</td>
+</tr>
+
+
+<!-- Body -->
+<tr>
+	<td>
+	<center>	
+	<div class="body1">
+	<table style="width:800px">
+	<tr>
+		<td class="body1">
+		As a Ph.D. student in the <a href="http://www.amsc.umd.edu/">Applied Mathematics & Statistics, and Scientific Computation</a> program at the University of Maryland, College Park I completed the <a href="http://www.amsc.umd.edu/programs/sc_curriculum.html">Scientific Computation</a> concentration, and defended my thesis in 2012.  
+		My research is in the field of <a href="http://en.wikipedia.org/wiki/Computer_vision">Computer Vision</a> under the supervision of <a href="http://www.cs.umd.edu/~djacobs/">Dr. David Jacobs</a>, Professor of Computer Science.  
+		I am interested in methods of shape representation as a subfield of object recognition.
+		My Ph.D. explored deformation and illumination insensitive object recognition algorithms based on measuring dense 2-D morphings between deformable objects such as faces.
+		<br><br>
+
+		I was recently been employed as an intern at the <a href="http://www.jhuapl.edu/">Johns Hopkins Applied Physics Lab</a>, 
+		where I was researching methods of estimating the pose of a known object which is seen by several cameras in a low-bandwidth sensor network, 
+		and was developing a dynamic model of the heart to aid in surgical planning.  Previously, I spent several summers writing surface optimization software with the Math and Computing Technology Research Group at <a href="http://www.boeing.com/">The Boeing Company</a>.
+		<br><br>
+
+		From January to July 2010 I was a visiting graduate researcher at the <a href="http://www.cmla.ens-cachan.fr/">Center for Mathematical Studies and their Applications</a> at the <a href="http://www.ens-cachan.fr/version-anglaise/">Ecole Normale Sup&eacute;rieure de Cachan</a> near Paris, France, studying deformable shape metrics under the direction of Professor <a href="http://atrouve.perso.math.cnrs.fr/">Alain Trouv&eacute;</a>.
+		<br><br>
+
+		Since defending my thesis, I have joined <a href="http://cvlab.epfl.ch/~fua/">Pascal Fua</a>'s Computer Vision Lab at the <a href="http://information.epfl.ch/glance">Ecole Polytechnique F&eacute;d&eacute;rale de Lausanne (EPFL)</a> in Lausanne, Switzerland as a postdoctoral researcher.
+
+
+		<br><br><br>
+
+		
+
+		<div class="h2"><b>Publications and Presentations</b></div><br>
+
+		A. Jorstad, D. Jacobs, A. Trouv&eacute;.  "A Fast Illumination and Deformation Insensitive Image Comparison Algorithm Using Wavelet-Based Geodesics."  European Conference on Computer Vision (ECCV), Oct. 2012.<br>
+		<a href="Files/JorstadECCV2012.pdf">pdf</a><br>
+        <a href="WaveletBasedGeodesicsCompare.php">Implementation in Matlab</a><br>
+        <br>
+            
+        A. Jorstad.  "Measuring Deformations and Illumination Changes in Images with Applications to Face Recognition."  Ph.D. Thesis, University of Maryland, College Park, Jul. 2012.<br>
+        <a href="Files/Jorstad_Thesis_2012.pdf">pdf</a><br>
+        <a href="Files/Jorstad_Defense.pdf">Talk</a><br>
+        <br>
+
+		A. Jorstad, D. Jacobs, A. Trouv&eacute;.  "A Deformation and Lighting Insensitive Metric for Face Recognition Based on Dense Correspondences."  Computer Vision and Pattern Recognition (CVPR), pp. 2353-2360, Jun. 2011.<br>
+		<a href="Files/JorstadCVPR2011.pdf">pdf</a><br>
+		<br>
+
+		A. Jorstad, D. Jacobs, A. Trouv&eacute;.  "A Deformation and Lighting Insensitive Metric for Face Recognition."  Poster, Women in Machine Learning Conference, Dec. 2010.<br>
+		<a href="Files/Jorstad_WiML_poster.pdf">pdf</a><br>
+		<br>
+
+		A. Jorstad, D. DeMenthon, I. Wang, P. Burlina. "Distributed Consensus on Camera Pose." Image Processing, IEEE Transactions on, vol. 19, pp. 2396-2407, Sep. 2010.<br>
+		<a href="Files/Distributed_Consensus_on_Camera_Pose.pdf">pdf</a><br>
+		<br>
+
+		P. Burlina, C. Sprouse, D. DeMenthon, A. Jorstad, R. Juang, F. Contijoch, T. Abraham, D. Yuh, E. McVeigh.  
+		"Patient-Specific Modeling and Analysis of the Mitral Valve Using 3D-TEE."  
+		Information Processing in Computer-Assisted Interventions (IPCAI), First International Conference, Jun. 2010; 
+		Lecture Notes in Computer Science, vol. 6135/2010, pp. 135-146, 2010.<br>
+		<a href="Files/Patient-Specific_Modeling_and_Analysis_of_the_Mitral_Valve_Using_3D-TEE.pdf">pdf</a><br>
+		<br>
+		
+		P. Burlina, C. Sprouse, D. DeMenthon, A. Jorstad, F. Contijoch, E. McVeigh, R. Juang, T. Abraham, D. Yuh.  
+		"Individualized Cardiothoracic Surgical Planning using Computer Aided 3D Modeling and Image Analysis."  
+		American Medical Association and the IEEE Engineering in Medicine and Biology Society Conference on Medical Technology, Mar. 2010.<br>
+		<a href="Files/AMA-IEEE_abstract.pdf">pdf</a><br>
+		<br>
+
+		Ph.D. Candidacy Exam, Department of Applied Mathematics & Statistics, and Scientific Computation, University of Maryland, College Park:  
+		"Robust Face Recognition Measuring 2D Deformations", May 2009.<br>
+		<a href="Files/Jorstad_Prospectus.pdf">pdf</a><br>
+		<a href="Files/Jorstad_Candidacy_Talk.pdf">Talk</a><br>
+		<br>
+
+		A. Jorstad, P. Burlina, I. Wang, D. Lucarelli, D. DeMenthon. "Model-Based Pose Estimation by Consensus." 
+		Intelligent Sensors, Sensor Networks and Information Processing (ISSNIP 2008), pp. 569-574, Dec. 2008.<br>
+		<a href="Files/Model-Based_Pose_Estimation_by_Consensus.pdf">pdf</a><br>
+		<br>
+
+		A. Jorstad, D. Jacobs. "Leaf Classification from Local Boundary Analysis." Poster, Graduate Research Interaction Day, University of Maryland, College Park, Apr. 2008.<br>
+		<a href="Files/Leaf_Classification_Poster.pdf">pdf</a><br>
+
+		</td>
+		
+	</tr>
+	</table>
+	</div>
+	</center>
+	</td>
+</tr>
+
+
+</table>
+
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-12400966-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+</body>
+</html>
